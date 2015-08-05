cb4934292801f9eb4e07d43c78d0b331663c4c7b
Sean Culver <sean@codingzeal.com>
2015-08-05 16:17:40 -0700

    Add RailsAdmin for production credits engine.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Non Sufia generated routes
   mount ProductionCredits::Engine, at: "/production_credits"
+  mount RailsAdmin::Engine => '/production_credits/admin', as: 'rails_admin'
 
   # Sufia generated routes
   blacklight_for :catalog
