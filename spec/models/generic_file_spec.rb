cac7cffa6e24a7c725ea8885e554380a693d5c27
Jason Harrison and Sean Culver <team+jason+sean@codingzeal.com>
2015-08-11 14:20:28 -0700

    Spec refactoring

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -1,64 +1,30 @@
 require 'rails_helper'
 
-describe GenericFile do
+shared_examples "a custom property" do |property_name|
+  let(:property_value) { "My Property" }
   let(:file) do
     GenericFile.create do |f|
       f.apply_depositor_metadata "user"
     end
   end
 
-  describe "has a production name property" do
-    let(:production_name) { "My Production" }
-
-    before do
-      file.production_name = production_name
-    end
-
-    subject { file.production_name }
-
-    it 'can access the property' do
-      is_expected.to eql production_name
-    end
-
-    it 'indexes the values' do
-      expect(file.to_solr[Solrizer.solr_name("production_name")]).to eq [production_name]
-    end
+  before do
+    file.send("#{property_name}=", property_value)
   end
 
-  describe "has a venue name property" do
-    let(:venue_name) { "My Venue" }
+  subject { file.send(property_name) }
 
-    before do
-      file.venue_name = venue_name
-    end
-
-    subject { file.venue_name }
-
-    it 'can access the property' do
-      is_expected.to eql venue_name
-    end
-
-    it 'indexes the values' do
-      expect(file.to_solr[Solrizer.solr_name("venue_name")]).to eq [venue_name]
-    end
+  it 'can access the property' do
+    is_expected.to eql property_value
   end
 
-  describe "has a work name property" do
-    let(:work_name) { "My Work" }
-
-    before do
-      file.work_name = work_name
-    end
-
-    subject { file.work_name }
-
-    it 'can access the property' do
-      is_expected.to eql work_name
-    end
-
-    it 'indexes the values' do
-      expect(file.to_solr[Solrizer.solr_name("work_name")]).to eq [work_name]
-    end
+  it 'indexes the values' do
+    expect(file.to_solr[Solrizer.solr_name(property_name)]).to eq [property_value]
   end
+end
 
+describe GenericFile do
+  it_behaves_like "a custom property", "production_name"
+  it_behaves_like "a custom property", "venue_name"
+  it_behaves_like "a custom property", "work_name"
 end
