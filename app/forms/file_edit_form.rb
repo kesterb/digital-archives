50dca2f2bdb3d94023583cbf55823d2cd44d81a9
Sean Culver <sean@codingzeal.com>
2015-08-26 17:13:48 -0700

    Less hacky implementation of resource_type singularization, updates to solr_document

--- app/forms/file_edit_form.rb
+++ app/forms/file_edit_form.rb
@@ -3,4 +3,11 @@ class FileEditForm < GenericFilePresenter
   include HydraEditor::Form::Permissions
 
   self.required_fields = [:title, :rights]
+
+  def self.build_permitted_params
+    permitted = super
+    permitted.delete(resource_type: [])
+    permitted << :resource_type
+    permitted
+  end
 end
