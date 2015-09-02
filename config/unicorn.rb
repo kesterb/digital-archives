774ed7d0b91b24261152eb62287162e1e0bd64fd
Joel Ferrier <joel@ferrier.io>
2015-09-02 10:16:29 -0700

    include unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+APP_PATH "/var/www/unicorn"
+working_directory APP_PATH
+
+pid APP_PATH + "/pids/unicorn.pid"
+
+stderr_path APP_PATH + "/log/unicorn.log"
+stdout_path APP_PATH + "/log/unicorn.log"
+
+listen APP_PATH + "/tmp/sockets/unicorn.sock"
+
+worker_processes 4
+
+timeout 600
+
+@resque_pid = nil
+
+before_fork do |server, worker|
+  @resque_pid ||= spawn("bundle exec /usr/local/bin/rake " + \
+  "resque-pool --environment production start")
+end
+
