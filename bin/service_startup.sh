36b26abc60ca6b1949d1ee37bc2240295d15823f
Joel Ferrier <joel@ferrier.io>
2015-09-02 18:52:56 -0700

    switch to system-wide rvm

--- bin/service_startup.sh
+++ bin/service_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/unicorn/.profile
+source /etc/profile.d/rvm.sh
 
 resque-pool --daemon --environment production start
 /var/www/unicorn/bin/bundle exec "unicorn_rails -c config/unicorn.rb -E production"
