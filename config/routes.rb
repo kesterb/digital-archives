5b4d0e37c6fd5a62a261633791c7f79279eef2c3
Randy Coulman <rcoulman@gmail.com>
2015-09-11 10:58:08 -0700

    Single -> double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,18 @@ Rails.application.routes.draw do
   blacklight_for :catalog
   devise_for :users
   Hydra::BatchEdit.add_routes(self)
-  # This must be the very last route in the file because it has a catch-all route for 404 errors.
-    # This behavior seems to show up only in production mode.
-  mount Sufia::Engine => '/admin'
+  # This must be the very last route in the file because it has a catch-all
+  # route for 404 errors.
+  # This behavior seems to show up only in production mode.
+  mount Sufia::Engine => "/admin"
 
   resources :searches
 
-  get "about" => 'cms_pages#show', id: 'about_page'
-  get "policies" => 'cms_pages#show', id: 'policies_page'
+  get "about" => "cms_pages#show", id: "about_page"
+  get "policies" => "cms_pages#show", id: "policies_page"
 
   # For editing via the Sufia backend
-  get "admin/policies" => 'pages#show', id: 'policies_page'
+  get "admin/policies" => "pages#show", id: "policies_page"
 
-  root to: 'searches#index'
+  root to: "searches#index"
 end
