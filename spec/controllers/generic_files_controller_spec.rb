c4b298a60a121889f488e8c4519991a0d932c88b
Randy Coulman <rcoulman@gmail.com>
2015-09-17 15:41:37 -0700

    Eliminate constant redefinition warning
    Apparently, defining a constant within a describe block actually
    defines it globally, which is not what I originally intended.
    
    I couldn’t find any other way to eliminate the fully-qualified names
    within the specs, so I’m going with that for now.  Including or
    extending the ProductionCredits module didn’t work, nor did using let.

--- spec/controllers/generic_files_controller_spec.rb
+++ spec/controllers/generic_files_controller_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe GenericFilesController do
-  Production = ProductionCredits::Production
-  Venue = ProductionCredits::Venue
-  Work = ProductionCredits::Work
-
   routes { Sufia::Engine.routes }
   let(:user) { FactoryGirl.find_or_create(:user) }
   let(:generic_file) do
@@ -13,10 +9,10 @@ describe GenericFilesController do
     end
   end
   let(:production) do
-    instance_double(Production, production_name: "The Production")
+    instance_double(ProductionCredits::Production, production_name: "The Production")
   end
-  let(:venue) { instance_double(Venue, name: "The Venue") }
-  let(:work) { instance_double(Work, title: "The Work") }
+  let(:venue) { instance_double(ProductionCredits::Venue, name: "The Venue") }
+  let(:work) { instance_double(ProductionCredits::Work, title: "The Work") }
   let(:production_id) { 1 }
   let(:venue_id) { 2 }
   let(:work_id) { 3 }
@@ -30,12 +26,12 @@ describe GenericFilesController do
   let(:reloaded) { generic_file.reload }
 
   before do
-    allow(Production).to receive(:find)
+    allow(ProductionCredits::Production).to receive(:find)
       .with([production_id.to_s]) { [production] }
-    allow(Production).to receive(:find).with([]) { [] }
-    allow(Venue).to receive(:find).with([venue_id.to_s]) { [venue] }
-    allow(Work).to receive(:find).with([work_id.to_s]) { [work] }
-    allow(Work).to receive(:find).with([]) { [] }
+    allow(ProductionCredits::Production).to receive(:find).with([]) { [] }
+    allow(ProductionCredits::Venue).to receive(:find).with([venue_id.to_s]) { [venue] }
+    allow(ProductionCredits::Work).to receive(:find).with([work_id.to_s]) { [work] }
+    allow(ProductionCredits::Work).to receive(:find).with([]) { [] }
     sign_in user
     post :update, id: generic_file, generic_file: attributes
   end
--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -1,35 +1,31 @@
 require 'rails_helper'
 
 describe GenericFile do
-  Production = ProductionCredits::Production
-  Venue = ProductionCredits::Venue
-  Work = ProductionCredits::Work
-
   subject(:file) do
     GenericFile.create do |f|
       f.apply_depositor_metadata "user"
     end
   end
   let(:production) do
-    instance_double(Production,
+    instance_double(ProductionCredits::Production,
     id: 42,
     production_name: "PRODUCTION",
     venue: production_venue,
     work: production_work
     )
   end
-  let(:venue) { instance_double(Venue, id: 58, name: "VENUE") }
-  let(:production_venue) { instance_double(Venue, name: "PRODUCTION VENUE") }
-  let(:work) { instance_double(Work, id: 123, title: "WORK") }
-  let(:production_work) { instance_double(Work, title: "PRODUCTION WORK") }
+  let(:venue) { instance_double(ProductionCredits::Venue, id: 58, name: "VENUE") }
+  let(:production_venue) { instance_double(ProductionCredits::Venue, name: "PRODUCTION VENUE") }
+  let(:work) { instance_double(ProductionCredits::Work, id: 123, title: "WORK") }
+  let(:production_work) { instance_double(ProductionCredits::Work, title: "PRODUCTION WORK") }
 
   before do
-    allow(Production).to receive(:find).with([production.id]) { [production] }
-    allow(Production).to receive(:find).with([]) { [] }
-    allow(Work).to receive(:find).with([work.id]) { [work] }
-    allow(Work).to receive(:find).with([]) { [] }
-    allow(Venue).to receive(:find).with([venue.id]) { [venue] }
-    allow(Venue).to receive(:find).with([]) { [] }
+    allow(ProductionCredits::Production).to receive(:find).with([production.id]) { [production] }
+    allow(ProductionCredits::Production).to receive(:find).with([]) { [] }
+    allow(ProductionCredits::Work).to receive(:find).with([work.id]) { [work] }
+    allow(ProductionCredits::Work).to receive(:find).with([]) { [] }
+    allow(ProductionCredits::Venue).to receive(:find).with([venue.id]) { [venue] }
+    allow(ProductionCredits::Venue).to receive(:find).with([]) { [] }
   end
 
   describe "associated production" do
