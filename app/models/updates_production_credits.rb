ec287322781f865ee8654d15baecbb60d062f0e2
Randy Coulman <rcoulman@gmail.com>
2015-09-17 16:59:42 -0700

    Move production credit update tests where they belong
    Also, fix the controller tests by using string IDs, not integers

--- app/models/updates_production_credits.rb
+++ app/models/updates_production_credits.rb
@@ -1,6 +1,4 @@
 class UpdatesProductionCredits
-  include ProductionCredits
-
   def self.update(generic_file)
     new(generic_file).update
   end
@@ -24,33 +22,33 @@ class UpdatesProductionCredits
   end
 
   def update_venues
-    generic_file.venue_ids = venues.map(&:id).compact
+    generic_file.venue_ids = venues.map(&:id).compact.map(&:to_s)
     generic_file.venue_names = venues.map(&:name).compact
   end
 
   def update_works
-    generic_file.work_ids = works.map(&:id).compact
+    generic_file.work_ids = works.map(&:id).compact.map(&:to_s)
     generic_file.work_names = works.map(&:title).compact
   end
 
   def productions
     @productions ||= begin
       ids = generic_file.production_ids
-      ids ? Production.find(ids).compact : []
+      ids ? ProductionCredits::Production.find(ids).compact : []
     end
   end
 
   def venues
     @venues ||= begin
       ids = generic_file.venue_ids || []
-      (ids.any? ? Venue.find(ids) : productions.map(&:venue)).compact
+      (ids.any? ? ProductionCredits::Venue.find(ids) : productions.map(&:venue)).compact
     end
   end
 
   def works
     @works ||= begin
       ids = generic_file.work_ids || []
-      (ids.any? ? Work.find(ids) : productions.map(&:work)).compact
+      (ids.any? ? ProductionCredits::Work.find(ids) : productions.map(&:work)).compact
     end
   end
 end
--- spec/controllers/generic_files_controller_spec.rb
+++ spec/controllers/generic_files_controller_spec.rb
@@ -9,37 +9,34 @@ describe GenericFilesController do
     end
   end
   let(:production) do
-    instance_double(ProductionCredits::Production, production_name: "The Production")
+    instance_double(ProductionCredits::Production, id: 1, production_name: "The Production")
   end
-  let(:venue) { instance_double(ProductionCredits::Venue, name: "The Venue") }
-  let(:work) { instance_double(ProductionCredits::Work, title: "The Work") }
-  let(:production_id) { 1 }
-  let(:venue_id) { 2 }
-  let(:work_id) { 3 }
+  let(:venue) { instance_double(ProductionCredits::Venue, id: 2, name: "The Venue") }
+  let(:work) { instance_double(ProductionCredits::Work, id: 3, title: "The Work") }
   let(:attributes) do
     {
-      production_ids: [production_id],
-      venue_ids: [venue_id],
-      work_ids: [work_id]
+      production_ids: [production.id],
+      venue_ids: [venue.id],
+      work_ids: [work.id]
     }
   end
   let(:reloaded) { generic_file.reload }
 
   before do
     allow(ProductionCredits::Production).to receive(:find)
-      .with([production_id.to_s]) { [production] }
+      .with([production.id.to_s]) { [production] }
     allow(ProductionCredits::Production).to receive(:find).with([]) { [] }
-    allow(ProductionCredits::Venue).to receive(:find).with([venue_id.to_s]) { [venue] }
-    allow(ProductionCredits::Work).to receive(:find).with([work_id.to_s]) { [work] }
+    allow(ProductionCredits::Venue).to receive(:find).with([venue.id.to_s]) { [venue] }
+    allow(ProductionCredits::Work).to receive(:find).with([work.id.to_s]) { [work] }
     allow(ProductionCredits::Work).to receive(:find).with([]) { [] }
     sign_in user
     post :update, id: generic_file, generic_file: attributes
   end
 
   it "persists the ids" do
-    expect(reloaded.production_ids).to eq [production_id.to_s]
-    expect(reloaded.venue_ids).to eq [venue_id.to_s]
-    expect(reloaded.work_ids).to eq [work_id.to_s]
+    expect(reloaded.production_ids).to eq [production.id.to_s]
+    expect(reloaded.venue_ids).to eq [venue.id.to_s]
+    expect(reloaded.work_ids).to eq [work.id.to_s]
   end
 
   it "finds and persists the names" do
