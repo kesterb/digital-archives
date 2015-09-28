8d0e411f928c9a4eacf4ddff2d7d1f79705695f8
Randy Coulman <rcoulman@gmail.com>
2015-09-28 10:28:50 -0700

    Yet more formatting
    Thanks Hound

--- app/helpers/generic_file_production_helper.rb
+++ app/helpers/generic_file_production_helper.rb
@@ -1,17 +1,17 @@
 module GenericFileProductionHelper
   def productions_for_select
     productions = ProductionCredits::Production.order(:production_name)
-    productions.map {|p| {"#{p.production_name} - #{p.open_on.year}" => p.id } }.reduce({}, :update)
+    productions.map { |p| { "#{p.production_name} - #{p.open_on.year}" => p.id } }.reduce({}, :update)
   end
 
   def works_for_select
     works = ProductionCredits::Work.order(:title)
-    works.map {|w| { w.title => w.id } }.reduce({}, :update)
+    works.map { |w| { w.title => w.id } }.reduce({}, :update)
   end
 
   def venues_for_select
     venues = ProductionCredits::Venue.order(:name)
-    venues.map {|v| { v.name => v.id } }.reduce({}, :update)
+    venues.map { |v| { v.name => v.id } }.reduce({}, :update)
   end
 
   def event_types_for_select
