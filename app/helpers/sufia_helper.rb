7cd87addd430402b4cc0936dc0b49447ff80bf1a
Randy Coulman and Sean Culver <team+randy+sean@codingzeal.com>
2015-09-30 15:22:52 -0700

    Show Discoverable badge where missing
    We needed to override the render_visibility_label helper to include
    discoverable.
    
    We discovered that, in one use case, the document is actually a
    `SolrDocument` and not a `GenericFile`, so we had to add
    `discoverable?` and friends to `SolrDocument` in order for this to work.

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -2,4 +2,17 @@ module SufiaHelper
   include ::BlacklightHelper
   include Sufia::BlacklightOverride
   include Sufia::SufiaHelperBehavior
+
+  def render_visibility_label(document)
+    case
+    when document.public?
+      content_tag :span, t("sufia.visibility.open"), class: "label label-success", title: t("sufia.visibility.open_title_attr")
+    when document.discoverable?
+      content_tag :span, t("digital_archives.visibility.discoverable"), class: "label label-warning", title: t("digital_archives.visibility.discoverable_title_attr")
+    when document.registered?
+      content_tag :span, t("sufia.institution_name"), class: "label label-info", title: t("sufia.institution_name")
+    else
+      content_tag :span, t("sufia.visibility.private"), class: "label label-danger", title: t("sufia.visibility.private_title_attr")
+    end
+  end
 end
