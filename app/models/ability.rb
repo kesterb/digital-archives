5f36cdb7f1f907792483039ac9217e9cfc226a76
Randy Coulman <rcoulman@gmail.com>
2015-10-03 15:08:09 -0700

    Use our branch of hydra-head
    We’ve submitted a PR for adding discover abilities to
    hydra-access-controls (part of hydra-head).  I made a new branch,
    `osf_integration`, off of v9.2.2 and then cherry-picked the PR commit
    into that branch.
    
    This commit removes the local versions of the PR code.
    
    It also makes use of one of the methods added as part of the PR in
    `solr_document.rb`.  This makes our implementation of `discover_groups`
    more like Sufia’s implementation of `read_groups` and friends.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,33 +4,13 @@ class Ability
 
   # Define any customized permissions here.
   def custom_permissions
-    discover_permissions
     override_download_permissions
   end
 
   private
 
-  # TODO: discover_permissions and friends should really be moved into
-  # hydra_access_controls.  We should fork that gem and make the change
-  # there instead, then submit it as a PR.
-  #
-  # override_download_permissions should stay here, but maybe there's a
-  # way to not duplicate the code from hydra_access_controls.
-
-  def discover_permissions
-    can :discover, String do |id|
-      test_discover(id)
-    end
-
-    can :discover, ActiveFedora::Base do |obj|
-      test_discover(obj.id)
-    end
-
-    can :discover, SolrDocument do |obj|
-      cache.put(obj.id, obj)
-      test_discover(obj.id)
-    end
-  end
+  # Maybe there's a way to not duplicate the code from hydra-access-controls
+  # in override_download_permissions.
 
   def override_download_permissions
     can :download, ActiveFedora::File do |file|
@@ -42,38 +22,5 @@ class Ability
         can? :read, parent_id
       end
     end
-  end
-
-  def test_discover(id)
-    Rails.logger.debug("[CANCAN] Checking discover permissions for user: #{current_user.user_key} with groups: #{user_groups.inspect}")
-    group_intersection = user_groups & discover_groups(id)
-    result = !group_intersection.empty? || discover_users(id).include?(current_user.user_key)
-    result
-  end
-
-  # read implies discover, so discover_groups is the union of read and discover groups
-  def discover_groups(id)
-    doc = permissions_doc(id)
-    return [] if doc.nil?
-    dg = read_groups(id) | (doc[self.class.discover_group_field] || [])
-    Rails.logger.debug("[CANCAN] discover_groups: #{dg.inspect}")
-    dg
-  end
-
-  # read implies discover, so discover_users is the union of read and discover users
-  def discover_users(id)
-    doc = permissions_doc(id)
-    return [] if doc.nil?
-    dp = read_users(id) | (doc[self.class.discover_user_field] || [])
-    Rails.logger.debug("[CANCAN] discover_users: #{dp.inspect}")
-    dp
-  end
-
-  def self.discover_group_field
-    Hydra.config.permissions.discover.group
-  end
-
-  def self.discover_user_field
-    Hydra.config.permissions.discover.individual
   end
 end
